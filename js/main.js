06fdd824ea8d9ffc1310701c13ce4db23e115347
Marcos Acosta <macosta1625@gmail.com>
2021-01-21 09:47:50 -0500

    Navbar

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-  console.log('here!');
+  $("#homeButton").on("click", () => {
+    $("#projectButton").removeClass("active");
+    $("#homeButton").addClass("active");
+    let projects = document.getElementById("aboutMe")
+    projects.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+  });
+  $("#projectButton").on("click", () => {
+    $("#homeButton").removeClass("active");
+    $("#projectButton").addClass("active");
+    let projects = document.getElementById("projects")
+    projects.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+  });
 });
