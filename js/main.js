f369a037013825fb01d8f145a19b0abfc323f4b6
Marcos Acosta <macosta1625@gmail.com>
2021-02-10 16:49:11 -0500

    Floating and typing

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
+  setTimeout(() => {
+    txt = 'Hi! I\'m Marcos.'
+    speed = 70
+    typeWriter("name", txt, speed);
+    typeWriter("nameShadow", txt, speed);
+    setTimeout(() => {
+      $("#mySite").fadeIn();
+    }, txt.length * 70 + 500);
+  }, 1000)
   $("#homeButton").on("click", () => {
     $("#projectButton").removeClass("active");
     $("#homeButton").addClass("active");
@@ -12,3 +21,19 @@ $(document).ready(function() {
     projects.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
   });
 });
+
+function typeWriter(id_, text_, speed_) {
+  let i = 0;
+  let txt = text_;
+  let speed = speed_;
+
+  function write() {
+    if (i < txt.length) {
+      document.getElementById(id_).innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(write, speed);
+    }
+  }
+
+  write();
+}
